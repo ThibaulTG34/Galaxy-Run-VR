2074b4035a0e20a5f1ebd65dd2691354e6d34883
Thibault <thibault3470@outlook.fr>
2023-11-10 14:13:53 +0100

    debut tirs

--- GalaxyRUN/Assets/Scripts/MouvementJoueur.cs
+++ GalaxyRUN/Assets/Scripts/MouvementJoueur.cs
@@ -7,45 +7,48 @@ public class MouvementJoueur : MonoBehaviour
 {
     //declaration des variables
     public float speed = 0.0001f;
-    public float Rotatespeed = 0.005f;
+    public float Bulletspeed = 20f;
+    public float Rotatespeed = 0.09f;
     public float jumpSpeed = 8f;
     public float gravity = 20f;
     public Camera cam;
     public float sensi = 30f;
     private Vector3 mouvement = Vector3.zero;
     CharacterController player;
-    public Animator animator;
+    public GameObject bullet;
+    public GameObject bullet1_pos;
+    public GameObject bullet2_pos;
 
     void Start()
     {
-        //recuperation du composant CharacterController
         player = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-        animator = GetComponent<Animator>();
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         mouvement = new Vector3(Input.GetAxisRaw("Vertical"), 0, -Input.GetAxisRaw("Horizontal"));
        
-        //recuperation des valeurs des axes horizontal et vertical 
-        //et stockage dans un Vector3
-        //mouvement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        //tient compte de la rotation du joueur
         mouvement = transform.TransformDirection(mouvement);
-        //on multiplie le vector3 par la vitesse de deplacement
-        mouvement *= speed;
-        //si la touche ESPACE est pressee
-        /*if (Input.GetButton("Jump"))
-        {
-            //on dit que notre point sur l'axe y augmente de la valeur de jumpSpeed 
-            mouvement.y = jumpSpeed;
-        }*/
-        
-        //ici on effectue la rotation de notre joueur s'il glisse la souris a gauche ou a droite
-        //transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * Time.deltaTime * speed * sensi);
+        //mouvement *= speed;
+        player.gameObject.transform.Rotate(mouvement);
+        player.Move(Vector3.forward * speed * Time.deltaTime);
 
-        player.gameObject.transform.Rotate(mouvement * Rotatespeed);
-        player.Move(Vector3.forward * speed);
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet1 = Instantiate(bullet, bullet1_pos.transform.position, Quaternion.identity) as GameObject;
+            newBullet1.transform.Rotate(new Vector3(90, 0, 0));
+            Rigidbody rBullet1 = newBullet1.GetComponent<Rigidbody>();
+            rBullet1.velocity = bullet1_pos.transform.TransformDirection(-cam.transform.forward) * Bulletspeed;
+
+            GameObject newBullet2 = Instantiate(bullet, bullet2_pos.transform.position, Quaternion.identity) as GameObject;
+            newBullet2.transform.Rotate(new Vector3(90, 0, 0));
+            Rigidbody rBullet2 = newBullet2.GetComponent<Rigidbody>();
+            rBullet2.velocity = bullet2_pos.transform.TransformDirection(-cam.transform.forward) * Bulletspeed;
+
+        }
     }
+
+
+
 }
